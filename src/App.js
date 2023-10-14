e9c2cf692f097b4f8041be020cfffa302e05727c
“limhaneul2244” <hnlim2244@gmail.com>
2023-10-14 19:12:02 +0900

    ✨ Feat: 🎉 프로젝트 시작

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import { GlobalStyle } from "./styles/GlobalStyle";
+
+function App() {
+  return (
+    <>
+      <GlobalStyle />
+      <div>
+        <h1>ggobook-coding❤</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/styles/GlobalStyle.js
@@ -0,0 +1,82 @@
+//reset css
+import { createGlobalStyle } from "styled-components";
+import reset from "styled-reset";
+import './fonts.css';
+
+export const GlobalStyle = createGlobalStyle`
+
+  ${reset};
+
+  html, body, div, span, applet, object, iframe,
+  h1, h2, h3, h4, h5, h6, p, blockquote, pre,
+  a, abbr, acronym, address, big, cite, code,
+  del, dfn, em, img, ins, kbd, q, s, samp,
+  small, strike, strong, sub, sup, tt, var,
+  b, u, i, center,
+  dl, dt, dd, ol, ul, li,
+  fieldset, form, label, legend,
+  table, caption, tbody, tfoot, thead, tr, th, td,
+  article, aside, canvas, details, embed,
+  figure, figcaption, footer, header, hgroup,
+  menu, nav, output, ruby, section, summary,
+  time, mark, audio, video {
+    margin: 0;
+    padding: 0;
+    border: 0;
+    font-size: 100%;
+    font: inherit;
+    vertical-align: baseline;
+  }
+
+  article, aside, details, figcaption, figure,
+  footer, header, hgroup, menu, nav, section {
+    display: block;
+  }
+  body {
+    font-family: 'SpoqaHanSansNeo-R';
+    line-height: 1;
+  }
+  ol, ul {
+    list-style: none;
+  }
+  blockquote, q {
+    quotes: none;
+  }
+  blockquote:before, blockquote:after,
+  q:before, q:after {
+    content: '';
+    content: none;
+  }
+  table {
+    border-collapse: collapse;
+    border-spacing: 0;
+  }
+  button {
+    background: unset;
+    border: unset;
+    cursor: pointer;
+  }
+  input {
+    background: unset;
+    border: unset;
+    font: inherit;
+  }
+  img {
+    display: block;
+    vertical-align: top;
+    width: 100%;
+  }
+  //숨김처리
+  .a11y {
+    clip: rect(1px, 1px, 1px, 1px);
+    clip-path: inset(50%);
+    width: 1px;
+    height: 1px;
+    margin: -1px;
+    overflow: hidden;
+    padding: 0;
+    position: absolute;
+  }
+
+  //추가로 적용해야할 공통 CSS 추가
+`
