acc20f5a8acee6d0e0ee6db90a8b03a6f7de173e
cutieun <rich.jieunlee@gmail.com>
2023-10-20 13:51:51 +0900

    🎨 style: Search 스타일 및 마크업 적용 완료

---
+++ src/pages/Search/User.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import { styled } from 'styled-components'
+
+const UserLayout = styled.div`
+    display: flex;
+    align-items: center;
+    gap: 12px;
+    margin: 20px 0 16px 0;
+    padding: 0 16px;
+
+    h3 {
+        padding-bottom: 6px;
+        font-size: 14px;
+    }
+
+    p {
+        font-size: 12px;
+        color: #767676;
+    }
+
+    img {
+        width: 50px;
+        height: 50px;
+        border-radius: 50%;
+        object-fit: cover;
+    }
+
+`
+
+
+export default function User() {
+  return (
+    <>
+        <UserLayout>
+            <img src="/images/img-profile-default.svg" alt="사용자 프로필 사진" />
+            <div>
+                <h3>애월읍 위니브 감귤농장</h3>
+                <p>@weniv_Mandarin</p>
+            </div>
+        </UserLayout>
+    </>
+  )
+}
