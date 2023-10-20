8aca683427ea23213fc717dc7a2247817d42566a
cutieun <rich.jieunlee@gmail.com>
2023-10-20 14:05:06 +0900

    🎨 style: FollowerList 스타일 및 마크업 적용 완료

---
+++ src/pages/FollowersList/FollowersList.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Follower from './Follower'
+// import Navigator from './Navigator'
+
+export default function FollowersList() {
+  return (
+    <>
+        <Follower></Follower>
+        <Follower></Follower>
+        <Follower></Follower>
+        <Follower></Follower>
+        <Follower></Follower>
+        <Follower></Follower>
+        <Follower></Follower>
+        <Follower></Follower>
+        <Follower></Follower>
+        <Follower></Follower>
+    </> 
+  )
+}
