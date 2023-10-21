c70779084073c5ca0c62c530f66f2dfca23d9186
Jaeyoung Lee <5061yo@naver.com>
2023-10-22 06:36:20 +0900

    ✨ feat: Spaces 컴포넌트 수정

--- src/components/Spaces/Spaces.jsx
+++ src/components/Spaces/Spaces.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import styled from "styled-components";
 
-export default function Spaces({ spaces }) {
-  return <StyledSpaces spaces={spaces} />;
+export default function Spaces({ gap }) {
+  return <StyledSpaces $gap={gap} />;
 }
 
 const StyledSpaces = styled.div`
-  margin-bottom: ${(props) => props.spaces};
+  margin-bottom: ${(props) => props.$gap};
 `;
