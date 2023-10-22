dab16e4e98fa59f6eb5342ec225aa532469074eb
Heesoo.J <kikl12345@naver.com>
2023-10-22 19:35:21 +0900

    🎨 style: 홈 화면 작성

--- src/App.js
+++ src/App.js
@@ -1,3 +1,11 @@
+import Home from "./pages/Home/Home";
+import Home2 from "./pages/Home/Home2";
+// import YourProfile from "./pages/YourProfile/YourProfile";
+// import YourProfile2 from "./pages/YourProfile/YourProfile2";
+// import YourProfile3 from "./pages/YourProfile/YourProfile3";
+// import YourProfile4 from "./pages/YourProfile/YourProfile4";
+// import Myprofile from "./pages/MyProfile/MyProfile"
+
 import { CommonLayOut, GlobalStyle, PaddingLayOut } from "./styles/GlobalStyle";
 
 function App() {
@@ -6,7 +14,9 @@ function App() {
       <GlobalStyle />
       <CommonLayOut>
         <PaddingLayOut padding={33}>
-          <h1>ggobook-coding❤</h1>
+          <>
+            <h1>ggobook-coding❤</h1>
+          </>
         </PaddingLayOut>
       </CommonLayOut>
     </>
