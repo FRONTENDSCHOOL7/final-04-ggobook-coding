6dd7f65de43324590f0c2d5420ab6702e44d349b
Jaeyoung Lee <5061yo@naver.com>
2023-10-24 09:50:22 +0900

    📝 docs: App.js 추가

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import { CommonLayOut, GlobalStyle } from "./styles/GlobalStyle";
+
+function App() {
+  return (
+    <>
+      <GlobalStyle />
+      <CommonLayOut></CommonLayOut>
+    </>
+  );
+}
+
+export default App;
