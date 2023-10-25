84ca28079f5d00ef4c057507805ba7c7b630d54d
“limhaneul2244” <hnlim2244@gmail.com>
2023-10-25 17:32:55 +0900

    ✨ feat: 버튼 활성/비활성화(컴포넌트로 이동 예정), onSubmit적용

--- src/pages/AddProduct/AddProduct.jsx
+++ src/pages/AddProduct/AddProduct.jsx
@@ -2,7 +2,9 @@ import React, { useCallback, useEffect, useRef, useState } from "react";
 import styled from "styled-components";
 import { CommonImgThumbnail } from "../../styles/GlobalStyle";
 import HeaderProfile from "../../components/Header/HeaderProfile";
-import Button from './../../components/Button/Button';
+import Button from "./../../components/Button/Button";
+
+const FormAddProductParent = styled.form``;
 
 const LayoutAddProduct = styled.ul`
   position: relative;
@@ -82,6 +84,7 @@ export default function AddProduct() {
   const TOKEN = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY1MzdjZGI2YjJjYjIwNTY2Mzg1ZjhlZCIsImV4cCI6MTcwMzM1MTM0MywiaWF0IjoxNjk4MTY3MzQzfQ.oJlrkrlk8XQSW17M24AL_csorLzsVXxvXzDc-3tFDyo`;
 
   const [addProductData, setAddProductData] = useState(null);
+  const [productName, setProductName] = useState(""); //상품명
   const [inputPrice, setInputPrice] = useState("");
   const [salesLink, setSalesLink] = useState("");
   const inputFocuseRef = useRef(null);
@@ -102,7 +105,6 @@ export default function AddProduct() {
       method: "POST",
       headers: {
         Authorization: `Bearer ${TOKEN}`,
-        // "Content-type": "multipart/form-data",
       },
       body: formData,
     });
@@ -112,8 +114,10 @@ export default function AddProduct() {
   //product API 함수
   const handleAddProductSubmit = useCallback(
     async (e) => {
+      e.preventDefault();
       try {
-        const uploadResult = await imgSubmit(); //이미지를 넣어주기 위해서 body전에 삽입하고 imgSubmit을 호출
+        //이미지를 넣어주기 위해서 body전에 삽입하고 imgSubmit을 호출
+        const uploadResult = await imgSubmit();
         console.log("uploadResult", uploadResult);
 
         const res = await fetch(`${URL}/product`, {
@@ -137,15 +141,14 @@ export default function AddProduct() {
           throw new Error("네트워크 문제가 발생했어요.");
         }
         const data = await res.json();
-        console.log('productData', data)
+        console.log("productData", data);
         setAddProductData(data);
       } catch (error) {
         if (!inputFocuseRef.current.value || !inputPrice || !salesLink) {
           console.error("🚫필수 입력사항을 모두 입력해주세요", error);
-        }else if (!Number(inputPrice)) {
+        } else if (!Number(inputPrice)) {
           console.error("🚫가격은 숫자로 입력하셔야 합니다.", error);
         }
-
       }
     },
     [inputPrice, salesLink, inputFocuseRef, TOKEN, imgSubmit]
@@ -157,7 +160,7 @@ export default function AddProduct() {
     return string;
   };
 
-  //가격, 링크 공통 input 함수
+  //상품명, 가격, 링크 공통 input 함수
   const handelInputAddProduct = useCallback((e) => {
     const { value, id } = e.target;
     const noComma = value.replaceAll(",", "");
@@ -171,17 +174,18 @@ export default function AddProduct() {
       setInputPrice(str);
     } else if (id === "link") {
       setSalesLink(value);
+    } else if (id === "name" && value.length <= 15) {
+      setProductName(value);
     }
   }, []);
 
   //로컬에서 이미지 등록
   const handleAddFileImg = useCallback(async (e) => {
     const file = e.target.files[0];
+
     setUploadImg(file);
     const reader = new FileReader();
-    console.log("이미지 로드 시작", file, reader);
-    //FileReader 객체의 readAsDataURL가 비동기로 동작하므로 await걸지 않아도 되며
-    //onload이벤트핸들러를 통해 어떤 처리를 할건지 진행한다.
+    console.log("AddProduct 이미지 로드 시작: ", file, reader);
     reader.readAsDataURL(file);
     reader.onload = () => {
       setAddFileImg(reader.result || null);
@@ -191,65 +195,81 @@ export default function AddProduct() {
   return (
     <>
       <HeaderProfile />
-      <LayoutAddProduct>
-        <button onClick={handleAddProductSubmit}>등록버튼</button>
-        <Button width="90px" height="32px" backgroundColor="--var()"/>
-        <LayoutInner>
-          <label>이미지 등록</label>
+      <FormAddProductParent onSubmit={handleAddProductSubmit}>
+        <LayoutAddProduct>
+          <Button
+            width="90px"
+            height="32px"
+            backgroundColor="var(--disabled)"
+            color="#fff"
+            type="submit"
+            disabled={
+              !inputPrice.length ||
+              !salesLink.length ||
+              !inputFocuseRef.current.value ||
+              !addFileImg
+            }
+          >
+            등록
+          </Button>
+          <LayoutInner>
+            <label>이미지 등록*</label>
 
-          <AddImgWrap>
-            <input
-              id="file"
-              className="btnUpload"
-              type="file"
-              name="file"
-              accept=".jpg, .jpeg, .png, .gif, .bmp, .tif, .heic"
-              onChange={handleAddFileImg}
-            />
-            <CommonImgThumbnail
-              src={addFileImg === "" ? null : addFileImg}
-              alt=""
-            />
-          </AddImgWrap>
-        </LayoutInner>
+            <AddImgWrap>
+              <input
+                id="file"
+                className="btnUpload"
+                type="file"
+                name="file"
+                accept=".jpg, .jpeg, .png, .gif, .bmp, .tif, .heic"
+                onChange={handleAddFileImg}
+              />
+              <CommonImgThumbnail
+                src={addFileImg === "" ? null : addFileImg}
+                alt=""
+              />
+            </AddImgWrap>
+          </LayoutInner>
 
-        <LayoutInner>
-          <label htmlFor="name">
-            상품명*
-            <input
-              id="name"
-              type="text"
-              maxLength={15}
-              ref={inputFocuseRef}
-              placeholder="2~15자 이내여야합니다."
-            />
-          </label>
-        </LayoutInner>
-        <LayoutInner>
-          <label htmlFor="price">
-            가격*
-            <input
-              id="price"
-              type="text"
-              value={comma(inputPrice)}
-              onChange={handelInputAddProduct}
-              placeholder="숫자만 입력가능합니다."
-            />
-          </label>
-        </LayoutInner>
-        <LayoutInner>
-          <label htmlFor="link">
-            판매 링크*
-            <input
-              id="link"
-              type="text"
-              value={salesLink}
-              onChange={handelInputAddProduct}
-              placeholder="URL을 입력해주세요"
-            />
-          </label>
-        </LayoutInner>
-      </LayoutAddProduct>
+          <LayoutInner>
+            <label htmlFor="name">
+              상품명*
+              <input
+                id="name"
+                type="text"
+                value={productName}
+                onChange={handelInputAddProduct}
+                ref={inputFocuseRef}
+                placeholder="2~15자 이내여야합니다."
+              />
+            </label>
+          </LayoutInner>
+          <LayoutInner>
+            <label htmlFor="price">
+              가격*
+              <input
+                id="price"
+                type="text"
+                value={comma(inputPrice)}
+                onChange={handelInputAddProduct}
+                placeholder="숫자만 입력가능합니다."
+              />
+            </label>
+          </LayoutInner>
+          <LayoutInner>
+            <label htmlFor="link">
+              판매 링크*
+              <input
+                id="link"
+                type="text"
+                value={salesLink}
+                onChange={handelInputAddProduct}
+                placeholder="URL을 입력해주세요"
+              />
+            </label>
+          </LayoutInner>
+        </LayoutAddProduct>
+      </FormAddProductParent>
     </>
   );
 }
