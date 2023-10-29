5e93ecc01069700084a95b360f27327086521d12
Jaeyoung Lee <5061yo@naver.com>
2023-10-30 02:01:35 +0900

    ✨ feat: ProfileEditBtn 컴포넌트 수정

--- src/components/ProfileEditBtn/ProfileEditBtn.jsx
+++ src/components/ProfileEditBtn/ProfileEditBtn.jsx
@@ -1,19 +1,64 @@
 import React from "react";
 import styled from "styled-components";
 
-export default function ProfileEditBtn({ marginTop }) {
-  return <StyledButton $marginTop={marginTop}></StyledButton>;
+export default function ProfileEditBtn({
+  type,
+  marginTop,
+  imgSrc,
+  onChange,
+  background,
+}) {
+  return (
+    <StyledLabel $marginTop={marginTop} $imgSrc={imgSrc}>
+      <ProfileImgSetting
+        type={type}
+        name="image"
+        accept="image/*"
+        onChange={onChange}
+      ></ProfileImgSetting>
+    </StyledLabel>
+    // <SetProfileImg
+    //   type={type}
+    //
+    //   $imgSrc={imgSrc}
+    // ></SetProfileImg>
+  );
 }
 
-const StyledButton = styled.button`
+// const SetProfileImg = styled.input`
+//   width: 110px;
+//   height: 110px;
+//   margin-top: ${(props) => (props.$marginTop ? props.$marginTop : "0")};
+//   position: absolute;
+//   /* background: url(${(props) =>
+//     props.$imgSrc ? props.$imgSrc : "images/img-profile-default.svg"};); */
+//   background-size: 110px;
+//   border-radius: 110px;
+//   border: 1px solid #dbdbdb;
+
+//   &::before {
+//     content: "";
+//     display: inline-block;
+//     width: 36px;
+//     height: 36px;
+//     position: absolute;
+//     bottom: 0;
+//     right: 0;
+//     background: url("images/upload-file.svg");
+//     background-size: 36px;
+//   }
+// `;
+
+const StyledLabel = styled.label`
   width: 110px;
   height: 110px;
-  margin-top: ${(props) => (props.$marginTop ? props.$marginTop : "0")};
+  border-radius: 50%;
   position: absolute;
-  background: url("images/img-profile-default.svg") no-repeat;
+  margin-top: ${(props) => (props.$marginTop ? props.$marginTop : "0")};
+  background: url(${(props) =>
+    props.$imgSrc ? props.$imgSrc : "images/img-profile-default.svg"});
   background-size: 110px;
-  border-radius: 110px;
-  border: 1px solid #dbdbdb;
+  cursor: pointer;
 
   &::before {
     content: "";
@@ -26,4 +71,8 @@ const StyledButton = styled.button`
     background: url("images/upload-file.svg");
     background-size: 36px;
   }
+`;
+
+const ProfileImgSetting = styled.input`
+  display: none;
 `;
