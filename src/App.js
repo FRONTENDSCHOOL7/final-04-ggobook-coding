856c4c2283fb169f4370e576a1d7acb8fe376272
“limhaneul2244” <hnlim2244@gmail.com>
2023-10-30 10:18:49 +0900

    ✨ feat: recoil적용에 따른 App.js 추가

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
+import { RecoilRoot } from 'recoil';
 import { CommonLayOut, GlobalStyle } from "./styles/GlobalStyle";
 import AppRouter from "./routes/AppRouter";
 
 function App() {
   return (
-    <>
+    <RecoilRoot>
       <GlobalStyle />
       <CommonLayOut>
         <AppRouter />
       </CommonLayOut>
-    </>
+    </RecoilRoot>
   );
 }
 
