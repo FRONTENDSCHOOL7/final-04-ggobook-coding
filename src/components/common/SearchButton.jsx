d046b73e08785bd15298ce6771fcd8d89dac0191
Jaeyoung Lee <5061yo@naver.com>
2023-11-01 03:15:58 +0900

    🎨 style: 공통 컴포넌트 마크업 및 스타일

---
+++ src/components/common/SearchButton.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import styled from "styled-components";
+
+export default function SearchButton({ onClick }) {
+  return <StyledSearchButton onClick={onClick}></StyledSearchButton>;
+}
+
+const StyledSearchButton = styled.button`
+  width: 25px;
+  height: 25px;
+  padding: 0;
+  background: url("images/icon-search.svg");
+  background-position: center;
+  background-size: 24px;
+`;
