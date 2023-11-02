d7b456c5715e2b8d526da68887cc7f9fe1217cbc
cutieun <rich.jieunlee@gmail.com>
2023-11-02 17:40:11 +0900

    ✨ feat: 안쓰는 hook 삭제

--- src/pages/PostPage/PostPage.jsx
+++ src/pages/PostPage/PostPage.jsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import { CommonBtn } from "../../styles/GlobalStyle";
 import { useNavigate, useParams } from "react-router-dom";
