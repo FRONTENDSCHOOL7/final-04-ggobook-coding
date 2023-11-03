1ef4e718859a156104db8399d4dfdd4114561864
hodolCup <kikl12345@naver.com>
2023-11-03 09:29:18 +0900

    ✨: 404 버튼 기능활성화, 가져오기용

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import { styled } from "styled-components";
+import { CommonBtn } from "../../styles/GlobalStyle";
 
 export default function NotFound() {
+  const navigate = useNavigate(); // useNavigate 초기화
+
+    // 뒤로 가기 기능을 처리하는 함수
+    const goBack = () => {
+      navigate(-1); // 이전 페이지로 이동
+    };
+
   const Page404Layout = styled.div`
     height: 100vh;
     display: flex;
@@ -37,9 +46,9 @@ export default function NotFound() {
   return (
     <Page404Layout>
       <div>
-        <img src="/images/img-profile-default.svg" alt="사용자 프로필 사진" />
+        <img src="/images/logo-404.svg" alt="사용자 프로필 사진" />
         <p>페이지를 찾을 수 없습니다. :(</p>
-        <button>이전 페이지</button>
+        <CommonBtn onClick={goBack}>이전 페이지</CommonBtn>
       </div>
     </Page404Layout>
   );
